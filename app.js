0f915ba9b41d75c61c7d18a022fa2e48a4ebdfc4
Alina <malinka2002@mail.ru>
2021-11-24 17:11:53 +0300

    new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./server/routes/router');
 const stylus =  require('stylus');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.set('views', './server/views');
 app.set('view engine', 'pug');
@@ -15,5 +15,5 @@ app.use(stylus.middleware({
 }));
 
 app.listen(PORT, (e) => {
-    console.log('Server runs at port ' + process.env.PORT + ' or 5000');
+    console.log('Server runs at port ' + process.env.PORT + ' or 4000');
 })
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
 const fs = require("fs");
 
+let data = '';
+const readFile = (path) => {
+    return fs.readFileSync(path, 'utf-8', (err, info) => {return info});
+}
+data = readFile('./data/market.csv');
+data = data.split('\n');
+// console.log(data);
+
+const Product = function(productsArr) {
+    let names = data[0].split(';');
+    names.forEach((name, i) => {this[name] = productsArr[i]});
+};
+
+const products = [];
+for (let i=1; i<data.length;i++) {
+    products.push(new Product(data[i].split(';')));
+}
+console.log(products);
+
+
 router.get('/', (rq, rs) => {
     rs.render('index', {
-        title: 'Здоровый образ жизни'
+        title: 'Здоровый образ жизни',
+        products: products
     });
 });
 
